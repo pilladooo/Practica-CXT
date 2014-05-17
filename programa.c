f6e655fb61e0ef61275ccdca175f69c70f500369
pilladooo <pilladooo@Pillado>
2014-05-17 19:06:00 +0200

    Añadida a función multiplicación ao programa

--- programa.c
+++ programa.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 
+// Añadida a función multiplicación
+int multiplicacion(int a, int b){
+	return a*b;
+}
+
 int main(){
 	int a = 2;
 	int b = 3;
 	int c = a*b;
 	printf("a = %i, b= %i, aXb= %i \n",a,b,c); 
+	printf("A multiplicación é %i\n",multiplicacion(a,b));
 return 0;
 }
+
 
