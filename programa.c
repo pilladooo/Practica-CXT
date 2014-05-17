6940e4e71f989257f226812b2a2e4206ab4253e0
pilladooo <pilladooo@Pillado>
2014-05-17 19:36:07 +0200

    Nova actualización e optimización do programa

--- programa.c
+++ programa.c
@@ -9,9 +9,12 @@ int main(){
 	int a = 2;
 	int b = 3;
 	int c;
-	c = multiplicacion(a,b)
+	c = multiplicacion(a,b);
+	printf("====================\n");
 	printf("a = %i, b= %i, aXb= %i \n",a,b,c);
+	printf("====================\n");
 return 0;
 }
+
 
 
