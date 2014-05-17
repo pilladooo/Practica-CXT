9f3993dc189458d1f43414a805aa0a22db8de885
pilladooo <pilladooo@Pillado>
2014-05-17 19:08:09 +0200

    Actualización da función multiplicacion do "programa.c"

--- programa.c
+++ programa.c
@@ -8,9 +8,9 @@ int multiplicacion(int a, int b){
 int main(){
 	int a = 2;
 	int b = 3;
-	int c = a*b;
-	printf("a = %i, b= %i, aXb= %i \n",a,b,c); 
-	printf("A multiplicación é %i\n",multiplicacion(a,b));
+	int c;
+	c = multiplicacion(a,b)
+	printf("a = %i, b= %i, aXb= %i \n",a,b,c);
 return 0;
 }
 
