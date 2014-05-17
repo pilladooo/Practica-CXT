2f47eb98a3076b90a1912eca458ee25b6ffd3650
pilladooo <pilladooo@Pillado>
2014-05-17 16:52:14 +0200

    Mellora e optimización do "programa.c"

--- programa.c
+++ programa.c
@@ -4,9 +4,7 @@ int main(){
 	int a = 2;
 	int b = 3;
 	int c = a*b;
-	printf("El número a = %i\n",a);
-	printf("El número b = %i\n",b);
-	printf("Multiplicación a(X)b = %i\n",c); 
+	printf("a = %i, b= %i, aXb= %i \n",a,b,c); 
 return 0;
 }
 
